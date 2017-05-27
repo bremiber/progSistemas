048945325ce0d8f18c4d6e7b1622f0076bb72800
bremiber <bremiber@espol.edu.ec>
2017-05-27 06:33:41 -0500

    Completo: validación número negativo

--- cifrado_parte1.c
+++ cifrado_parte1.c
@@ -1,70 +1,107 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #define TAMANIO 1024
 void cifrar(char a[TAMANIO], int b);
-void validar_entero(char numero[]);
+bool validar_entero(char numero[]);
 
-int main(){
+
+int main()
+{
+	 
 	char mensaje[TAMANIO];
 	char num[20];
 	int llave;
 	bool valido=false;
 
-	printf("Cifrado C%cclico\n",161);
-	printf("Ingrese mensaje a cifrar: ");
-	fgets(mensaje,TAMANIO,stdin);
 
+	printf("++++++++++++++++++++++++++\n");
+	printf("+++  Cifrado C%cclico   +++\n",161);
+	printf("++++++++++++++++++++++++++\n");
+	printf("Ingrese mensaje a cifrar: ");
+	fgets(mensaje, TAMANIO, stdin); 
+	/*printf("el mensaje es: %s\n", mensaje);*/
 	do{
 		printf("Ingrese la llave num%crica: ",130);
 		scanf("%s",num);
 		valido=validar_entero(num);
 	}while(valido==false);
-
+	
 	llave=atoi(num);
-
+	
 	cifrar(mensaje,llave);
 	return 0;
 }
-
 
 void cifrar(char a[TAMANIO], int b){
 	int i=0;
 	int resta=0;
 	int desp=0;
-	while(a[i]!='\0'){
-		if((a[i]>='A') && (a[i]<='Z')){
-			resta='Z'-a[i];
-			if(b>resta){
-				desp=b-resta-1;
-				a[i]='A'+desp;
+	int dif=0;
+	if (b<0){
+		while(a[i]!='\0'){
+			if ((a[i]>='A') && (a[i]<='Z')){
+				resta=a[i]-'A';
+				if (abs(b)>resta){
+					dif=abs(b)-resta;
+					a[i]='Z'-dif+1;
+				}
+				else{
+					a[i]=a[i]+b;
+				}
 			}
-			else{
-				a[i]=a[i]+b;
+			else if((a[i]>='a') && (a[i]<='z')){
+				resta=a[i]-'a';
+				if (abs(b)>resta){
+					dif=abs(b)-resta;
+					a[i]='z'-dif+1;
+				}
+				else{
+					a[i]=a[i]+b;
+				}
 			}
+			i++;
 		}
-		else if((a[i]>='a') && (a[i]<='z')){
-			resta='z'-a[i];
-			if(b>resta){
-				desp=b-resta-1;
-				a[i]='a'+desp;
+	}else{
+		while(a[i]!='\0'){
+			if ((a[i]>='A') && (a[i]<='Z')){
+				resta='Z'-a[i];
+				if (b>resta){
+					desp=b-resta-1;
+					a[i]='A'+desp;
+				}
+				else{
+					a[i]=a[i]+b;
+				}
 			}
-			else{
-				a[i]=a[i]+b;
+			else if((a[i]>='a') && (a[i]<='z')){
+				resta='z'-a[i];
+				if (b>resta){
+					desp=b-resta-1;
+					a[i]='a'+desp;
+				}
+				else{
+					a[i]=a[i]+b;
+				}
 			}
+			i++;
 		}
-		i++;
 	}
-	printf("\nMensaje cifrado: %s\n",a);
+	printf("\nMensaje cifrado: %s\n", a);
 }
-
 
 bool validar_entero(char numero[]){
 	int i;
-	for(i=0; i<strlen(numero);i++){
-		if(!(numero[i]>48 && numero[i]<=57)){
-			printf("Ingrese numero....\n");
+	if (!(numero[0]==45) || ((numero[0]>=48 && numero[0]<=57)))
+		{
+			printf("Ingrese numero...\n");
+			return false;
+		}
+	for(i=1; i<strlen(numero);i++){
+
+		if(!(numero[i]>=48 && numero[i]<=57)){
+			printf("Ingrese numero...\n");
 			return false;
 		}
 	}
